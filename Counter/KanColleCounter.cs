9ad49b802c88692463040355bf853f9696f56cea
Grabacr07 <grabacr07+github@gmail.com>
2014-06-29 05:34:29 +0900

    ツール系プラグインの実装 (途中)

---
+++ Counter/KanColleCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Composition;
+
+namespace Counter
+{
+	[Export(typeof(IToolPlugin))]
+	public class KanColleCounter : IToolPlugin
+	{
+
+
+
+
+		public object GetSettingsView()
+		{
+			return null;
+		}
+
+		public object GetToolView()
+		{
+			return null;
+		}
+	}
+}
